ec3b807d2b0e8dab42a0bb2156055d024f0d393a
datamarouan <marouan.elmoussaoui@cfwb.be>
2022-04-21 22:37:10 +0200

    modif dans l'app

--- src/catalogue/views.py
+++ src/catalogue/views.py
@@ -4,12 +4,32 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import CreateView, UpdateView, DetailView, ListView, DeleteView
+from django.views.generic import CreateView, UpdateView, DetailView, ListView, DeleteView, TemplateView
 
 def index(request):
 	adresses = GeoRefPostalAddress.objects.all()
 	context = {'adresses':adresses}
 	return render(request, 'catalogue/accueil.html', context)
+
+#########
+# CUBIM #
+#########
+class CubimView(TemplateView):
+	template_name = "catalogue/cubim/accueil.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		return context
+
+class TfcView(TemplateView):
+	template_name = "catalogue/cubim/projet1.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		projet = kwgOrganization.objects.all().get(name="Académie des Beaux-Arts de la Ville de Tournai")
+		context['projet'] = projet
+		context['adresse'] = GeoRefPostalAddress.objects.all().get(of_organization=projet.identification)
+		return context
 
 ########
 # FASE #
